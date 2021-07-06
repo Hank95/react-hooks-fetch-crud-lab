d058c56f4e55493d6eb3697f43108b73fd62208e
github username <github email address>
2021-07-06 14:45:17 -0400

    commit tree 7ced7b8020e4617d94359c6d703fe073c1193169

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questionsData, setQuestionsData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((res) => res.json())
+      .then((json) => {
+        setQuestionsData(json);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
+  const addNewData = (newData) => {
+    const updatedData = [...questionsData, newData];
+    setQuestionsData(updatedData);
+  };
+
+  const handleDelete = (id) => {
+    console.log(id);
+    const updatedData = questionsData.filter((item) => item.id !== id);
+    setQuestionsData(updatedData);
+  };
+  const handleNewChange = (id) => {
+    const updatedData = questionsData.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          correctIndex: id,
+        };
+      } else {
+        return item;
+      }
+    });
+    setQuestionsData(updatedData);
+  };
+
+  console.log(questionsData);
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm sendNewData={addNewData} />
+      ) : (
+        <QuestionList
+          questions={questionsData}
+          deleteClick={handleDelete}
+          onNewChange={handleNewChange}
+        />
+      )}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,19 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ questions, deleteClick, onNewChange }) {
+  const questionsList = questions.map((q) => (
+    <QuestionItem
+      key={q.id}
+      question={q}
+      deleter={deleteClick}
+      onNewChange={onNewChange}
+    />
+  ));
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionsList}</ul>
     </section>
   );
 }
